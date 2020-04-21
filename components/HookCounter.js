fbfa83b0d1fa42549eae6b18efaa8fb55bbb43b7
cs2252 <chandansingh1896@gmail.com>
2020-04-21 15:11:20 +0530

    commit messsage

---
+++ components/HookCounter.js
@@ -0,0 +1,12 @@
+import React,{useState} from 'react'
+
+function HookCounter() {
+    const [state, setstate] = useState(0)
+    return (
+        <div>
+            <button onClick={()=>setstate(state+1)}>count {state}</button>
+        </div>
+    )
+}
+
+export default HookCounter
